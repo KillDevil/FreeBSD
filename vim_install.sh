66ccba085f38e4ba2478b489173ed86baf874e9c
ypyf <t34@qq.com>
2015-08-28 16:20:11 +0800

    add apt sources

--- vim_install.sh
+++ vim_install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
+#CentOS
 #yum install ncurses-devel
 #yum install python-devel
 #yum install lua-devel
+apt-get install libncurses5-dev
+apt-get install python-dev
+apt-get install liblua5.1-dev
 ./configure \
   --disable-gui --enable-fontset --disable-xsmp \
   --enable-python3interp --enable-rubyinterp=dynamic --enable-pythoninterp \
